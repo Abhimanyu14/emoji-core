d81509358f7a7f79c46b6ece936f67857aa56da6
Abhimanyu14 <abhimanyu.n14@gmail.com>
2024-04-06 15:19:05 +0530

    [TECH] library version upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -50,7 +50,7 @@ project.afterEvaluate {
             create<MavenPublication>("release") {
                 groupId = "com.github.Abhimanyu14"
                 artifactId = "emoji-core"
-                version = "1.0.12"
+                version = "1.0.13"
 
                 // Publish both the main JAR and the sources JAR
                 from(components["kotlin"])
